cbc18c6102d0fb4fe53c32da88bfb4c221f8d4a3
Murat194 <muratperi2006@gmail.com>
2024-11-27 22:37:53 +0700

    Add files via upload

---
+++ chapter 7/BFS.py
@@ -0,0 +1,32 @@
+def bfs(graph, start):
+    visited = set()
+    queue = [start]
+    result = []
+    
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            result.append(vertex)
+            queue.extend(neighbor for neighbor in graph[vertex] if neighbor not in visited)
+    
+    return result
+
+# Тестовый граф в виде словаря смежности
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F', 'G'],
+    'D': ['B'],
+    'E': ['B', 'H'],
+    'F': ['C'],
+    'G': ['C', 'I'],
+    'H': ['E'],
+    'I': ['G']
+}
+
+# Тестовый пример
+start_vertex = 'A'
+result = bfs(graph, start_vertex)
+print("Обход в ширину:", result)
+# Ожидаемый результат: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
